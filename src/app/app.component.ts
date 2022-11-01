7bf25c158952abebbc508fccbffa06df6ab13c17
Lazar, Viktor <viktor.lazar@sap.com>
2022-11-01 16:24:57 +0100

    ngx-dropzone and mat-stepper added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'neuralStyleFE';
+  title = "neuralStyleFE";
+  stylefile: File | undefined;
+  contentfile: File | undefined;
+
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ["", Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ["", Validators.required],
+  });
+  isLinear = false;
+
+  constructor(private _formBuilder: FormBuilder) {}
+
+  onSelectStyleFile(event: { addedFiles: any }) {
+    console.log(event);
+    this.stylefile = event.addedFiles[0];
+  }
+
+  onRemoveStyleFile(event: File) {
+    console.log(event);
+    this.stylefile = undefined;
+    //this.files.splice(this.files.indexOf(event), 1);
+  }
+
+  onSelectContentFile(event: { addedFiles: any }) {
+    console.log(event);
+    this.contentfile = event.addedFiles[0];
+    //this.files.push(...event.addedFiles);
+  }
+
+  onRemoveContentFile(event: File) {
+    console.log(event);
+    this.contentfile = undefined;
+    //this.files.splice(this.files.indexOf(event), 1);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgxDropzoneModule } from "ngx-dropzone";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatStepperModule } from "@angular/material/stepper";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxDropzoneModule,
+    BrowserAnimationsModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
